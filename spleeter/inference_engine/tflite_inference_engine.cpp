e602fb640293079b5d923839bc15b0db6538b957
Jinay Patel <jinay1991@gmail.com>
2020-05-09 20:16:56 +0000

    Rename tools to scripts

--- spleeter/inference_engine/tflite_inference_engine.cpp
+++ spleeter/inference_engine/tflite_inference_engine.cpp
@@ -101,27 +101,6 @@ void TFLiteInferenceEngine::Init()
     tflite::InterpreterBuilder(*model_, resolver)(&interpreter_);
     ASSERT_CHECK(interpreter_) << "Failed to construct interpreter";
 
-    SPLEETER_LOG(INFO) << "tensors size: " << interpreter_->tensors_size();
-    SPLEETER_LOG(INFO) << "nodes size: " << interpreter_->nodes_size();
-    SPLEETER_LOG(INFO) << "inputs: " << interpreter_->inputs().size();
-    SPLEETER_LOG(INFO) << "input(0) name: " << interpreter_->GetInputName(0);
-
-    int tensor_size = interpreter_->tensors_size();
-    for (int i = 0; i < tensor_size; i++)
-    {
-        if (interpreter_->tensor(i)->name)
-        {
-            SPLEETER_LOG(INFO) << i << ": " << interpreter_->tensor(i)->name << ", " << interpreter_->tensor(i)->bytes
-                               << ", " << interpreter_->tensor(i)->type << ", " << interpreter_->tensor(i)->params.scale
-                               << ", " << interpreter_->tensor(i)->params.zero_point;
-        }
-    }
-
-    const auto inputs = interpreter_->inputs();
-    const auto outputs = interpreter_->outputs();
-    SPLEETER_LOG(INFO) << "number of inputs: " << inputs.size();
-    SPLEETER_LOG(INFO) << "number of outputs: " << outputs.size();
-
     ASSERT_CHECK_EQ(interpreter_->AllocateTensors(), TfLiteStatus::kTfLiteOk) << "Failed to allocate tensors!";
 
     PrintInterpreterState(interpreter_.get());
